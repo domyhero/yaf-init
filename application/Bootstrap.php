71d40113c34829b71cfaf29c95fbc2338051c55e
bool <81001985@qq.com>
2017-01-03 22:54:14 +0800

    init

---
+++ application/Bootstrap.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @name Bootstrap
+ * @author bool
+ * @desc 所有在Bootstrap类中, 以_init开头的方法, 都会被Yaf调用,
+ * @see http://www.php.net/manual/en/class.yaf-bootstrap-abstract.php
+ * 这些方法, 都接受一个参数:Yaf\Dispatcher $dispatcher
+ * 调用的次序, 和申明的次序相同
+ */
+class Bootstrap extends Yaf\Bootstrap_Abstract{
+
+    public function _initConfig() {
+		//把配置保存起来
+		$arrConfig = Yaf\Application::app()->getConfig();
+		Yaf\Registry::set('config', $arrConfig);
+	}
+
+	public function _initPlugin(Yaf\Dispatcher $dispatcher) {
+		//注册一个插件
+		$objSamplePlugin = new SamplePlugin();
+		$dispatcher->registerPlugin($objSamplePlugin);
+	}
+
+	public function _initRoute(Yaf\Dispatcher $dispatcher) {
+		//在这里注册自己的路由协议,默认使用简单路由
+		$router=$dispatcher->getInstance()->getRouter();
+      	// $route = new Yaf\Route\Simple("m", "c", "a");
+      	// $router->addRoute("simple", $route);
+      	$router->addConfig( Yaf\Registry::get('config')->routes );
+	}
+	
+	public function _initView(Yaf\Dispatcher $dispatcher){
+		//在这里注册自己的view控制器，例如smarty,firekylin
+		// $dispatcher->getInstance()->disableView();
+	}
+
+	public function _initResponse(Yaf\Dispatcher $dispatcher){
+		// $dispatcher->getInstance()->returnResponse(true);
+	}
+
+	public function _initSession(Yaf\Dispatcher $dispatcher){
+		session_id() || session_start();
+	}
+
+	public function _initCharset(Yaf\Dispatcher $dispatcher){
+		header("Content-Type: text/html;charset=utf-8");
+	}	
+
+
+}
